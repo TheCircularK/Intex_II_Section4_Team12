1486338073c65ba0cdde8bd1052cbac28857b027
ajbyu <ajames26@byu.edu>
2023-04-10 16:03:54 -0600

    Add project files.

---
+++ Intex_II_Section4_Team12/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intex_II_Section4_Team12.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
