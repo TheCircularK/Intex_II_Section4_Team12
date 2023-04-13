3dd30993c0dbe98e8bd8e850f1f7620d6fa6e8ad
nbain00 <zink47@gmail.com>
2023-04-13 01:05:38 -0600

    set up mssql server secrets manager

---
+++ Intex_II_Section4_Team12/Data/Migrations/20230413065001_Second.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intex_II_Section4_Team12.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
