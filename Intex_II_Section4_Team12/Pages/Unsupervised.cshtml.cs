7cdf6f6cbe3f939b308f3a9ce06c5c0d80b9a0ca
parkergeorge13 <parkergeorge13@gmail.com>
2023-04-13 00:54:29 -0600

    added unsupervised analysis page

---
+++ Intex_II_Section4_Team12/Pages/Unsupervised.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intex_II_Section4_Team12.Pages
+{
+    public class UnsupervisedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
