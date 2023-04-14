d4f8de477f76e9da03d80cef8c6e1261b96818f2
parkergeorge13 <parkergeorge13@gmail.com>
2023-04-13 21:45:42 -0600

    Fixed styling and some other stuff.

--- Intex_II_Section4_Team12/Models/MyApiRequestData.cs
+++ Intex_II_Section4_Team12/Models/MyApiRequestData.cs
@@ -4,23 +4,16 @@ namespace Intex_II_Section4_Team12.Models
 {
     public class MyApiRequestData
     {
-        [Required]
         public float Length { get; set; }
-        [Required]
         public float Depth { get; set; }
-        [Required]
         public float WestToHead { get; set; }
-        [Required]
         public string AgeAtDeath { get; set; }
         public float AgeAtDeath_other { get; set; }
         public float AgeAtDeath_adult { get; set; }
         public float AgeAtDeath_child { get; set; }
         public float AgeAtDeath_infant { get; set; }
-        [Required]
         public float WestToFeet { get; set; }
-        [Required]
         public float SouthToHead { get; set; }
-        [Required]
         public string Wrapping { get; set; }
         public float Wrapping_full { get; set; }
         public float Wrapping_partial { get; set; }
