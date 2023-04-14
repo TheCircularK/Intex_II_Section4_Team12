f9ad85ad466bece7f76635951ea7fb8ba7936d9f
TheCircularK <ajames1447@gmail.com>
2023-04-13 22:13:51 -0600

    Redid changes for posting to the prediction api.

---
+++ Intex_II_Section4_Team12/Models/MyApiRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Intex_II_Section4_Team12.Models
+{
+    public class MyApiRequestData
+    {
+        [Required]
+        public float Length { get; set; }
+        [Required]
+        public float Depth { get; set; }
+        [Required]
+        public float WestToHead { get; set; }
+        [Required]
+        public string AgeAtDeath { get; set; }
+        public float AgeAtDeath_other { get; set; }
+        public float AgeAtDeath_adult { get; set; }
+        public float AgeAtDeath_child { get; set; }
+        public float AgeAtDeath_infant { get; set; }
+        [Required]
+        public float WestToFeet { get; set; }
+        [Required]
+        public float SouthToHead { get; set; }
+        [Required]
+        public string Wrapping { get; set; }
+        public float Wrapping_full { get; set; }
+        public float Wrapping_partial { get; set; }
+        public float Wrapping_none { get; set; }
+    }
+}
---
+++ Intex_II_Section4_Team12/Repositories/IPhotoRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Intex_II_Section4_Team12.Repositories
+{
+    public interface IPhotoRepository
+    {
+        ICollection<string> Photos(string BurialNumberId);
+    }
+}
